45087168dd11e3da37c502e9eac0d48050f5bf1f
TheeJoshhh <joshua.billing9@gmail.com>
2019-09-19 18:41:10 +1200

    Initial Commit

---
+++ events/ready.js
@@ -0,0 +1,3 @@
+module.exports = (client) => {
+    console.log(`Status Notifier is online in ${client.guilds.size} guilds.`)
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+// Node Modules
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs")
+
+// Create Client
+const client = new Discord.Client();
+
+// Bind some thinggys to client for global access to said thinggys
+client.config = require('./config.json');
+client.settings = new Enmap({name: "settings"});
+
+
+// Load Commands into cache
+const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const load = async () => {
+    cmdFiles.forEach(file => {
+        try {
+            const f = require(`./commands/${file}`);
+            client.commands.set(f.help.name, f);
+        } catch (e) { 
+            console.log(`Error in command ${file}`, e.stack);
+        }
+    });
+};
+
+
+// Load and bind events to event files in the event folder.
+const evtFiles = fs.readdirSync("./events/").filter(file => file.endsWith('.js'));
+evtFiles.forEach(file => {
+    const evtName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+    client.on(evtName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+})
+
+
+// Login to the bot user
+client.login(client.config.token);
+
+// Load the commands
+load();
