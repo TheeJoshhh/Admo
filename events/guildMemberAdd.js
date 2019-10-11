7fbb8bd9b47c78779f5a3d6fabe53efc3354d949
TheeJoshhh <joshua.billing9@gmail.com>
2019-10-11 21:16:49 +1300

    Created guildMemberAdd event
    This event is currently only used for the autorole but in future will possible have a new member log.

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,13 @@
+module.exports = (client, member) => {
+
+    if (client.settings.get(member.guild.id, "autoRole.enabled") === true) {
+        if (member.guild.roles.find(role => role.id === client.settings.get(member.guild.id, "autoRole.role"))) {
+            return member.roles.add(member.guild.roles.find(role => role.id === client.settings.get(member.guild.id, "autoRole.role")))
+        } else {
+            client.settings.set(member.guild.id, '', "autoRole.role")
+            client.settings.set(member.guild.id, false, "autoRole.enabled")
+            client.functions.log(client, member.guild, "Error: Couldn't find AutoRole role.", "Auto-Role has been disabled since a member has joined the server and the auto-role role couldn't be found. Run `*setup autorole` to set it up again")
+        }
+    }
+
+}
