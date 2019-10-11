afd18cdd0236968dec4410cad4e6b145360b18ba
TheeJoshhh <joshua.billing9@gmail.com>
2019-10-11 21:13:02 +1300

    Created setup command
    - Still a WIP

---
+++ commands/setup.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args) => {
+    
+    let embed = new Discord.MessageEmbed();
+    embed.setColor(client.settings.get(message.guild.id, "embedColour"));
+
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Only Administrators can use this command.");
+
+    if (!args[0]) {
+
+    } else if (args[0].toLowerCase() === "mute" || args[0].toLowerCase() === "muterole") {
+        client.functions.setup_mute_channel(client, message)
+    } else if (args[0].toLowerCase() === "autorole" || args[0].toLowerCase() === "auto-role") {
+        client.functions.setup_autorole(client, message)
+    } else if (args[0].toLowerCase() === 'prefix') {
+        client.functions.setup_prefix(client, message)
+    } else if (args[0].toLowerCase() === 'modlogs') {
+        client.functions.setup_modLogs(client, message)
+    }
+
+}
+
+  module.exports.help = {
+        name: "setup",
+        aliases: ["configure", "reconfigure"],
+        class: "utility",
+        usage: `*setup [ThingToSetup(optional)]`,
+        description: "Setup or Reconfigure Admo in your server.",
+        status: "Working",
+        guildOnly: true
+    }
